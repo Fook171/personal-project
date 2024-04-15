445fffdc4551edb81a3d21fa9ee647de113a6718
Fook171 <94742809+Fook171@users.noreply.github.com>
2024-04-15 21:33:27 +0700

    Delete src directory

--- src/main/java/com/charitydonation/charityfund/repository/CampaignRepository.java
+++
@@ -1,7 +0,0 @@
-package com.charitydonation.charityfund.repository;
-
-import com.charitydonation.charityfund.model.Campaign;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampaignRepository extends JpaRepository<Campaign, Long> {
-}
